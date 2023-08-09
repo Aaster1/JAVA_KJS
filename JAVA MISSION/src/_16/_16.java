c7660aef529d78430c36255b2acef9c0cfeb134a
Aster1 <Administrator@221.154.127.148>
2023-08-09 20:42:58 +0900

    temp

--- JAVA MISSION/src/_16/_16.java
+++ JAVA MISSION/src/_16/_16.java
@@ -28,12 +28,10 @@ public class _16 {
 			System.err.println("에러남ㅋㅋ");
 			e.printStackTrace();
 		}
-//			finally {
 ////			1. Scanner closed 가 문제였다. 기존의 위치(try문 마지막열)에서 닫아버리면 객체가 해제되므로, 사용할 수 없다.
 ////			2. File 자료형으로 file을 받아올 때는 경로명으로 받아오자.
+//				3. 나의 경우, sc.close()가 try문의 마지막순번으로, while문 밖에 있는줄로만 알았는데 알고보니 while문 안쪽에 있었더라. 
+//				4. 그리고 그런 간단한 실수도 깨닫지 못했던 이유는 예외명을 확인하지 않아서다. 꼭 printStackTrace()를 작성하자.
 //			
-//			if(sc != null)
-//			sc.close();
-//		}
 	}
 }
